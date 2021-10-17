c692db0b8d3212ba4ac22cc8010e314a3b2c426a
Annika <annika0uwu@gmail.com>
2021-10-16 17:01:39 -0700

    Add directory parsing logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+// Command-line program to manage PS battle logs.
+
+mod directory_parse;
+
+#[derive(Debug)]
+pub enum BattleToolsError {
+    IOError(std::io::Error),
+}
+
+impl From<std::io::Error> for BattleToolsError {
+    fn from(error: std::io::Error) -> Self {
+        BattleToolsError::IOError(error)
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
