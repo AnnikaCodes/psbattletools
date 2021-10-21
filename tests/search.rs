d57c134657d5e5ed07539d0bcb440867f945dedf
Annika <annika0uwu@gmail.com>
2021-10-20 18:13:44 -0700

    Fix CI

--- tests/search.rs
+++ tests/search.rs
@@ -88,7 +88,14 @@ fn test_search_wins_only() {
 fn bench_search_1k(b: &mut Bencher) {
     build_test_dir(1_000).unwrap();
     let mut path = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
-    path.push("target/release/psbattletools");
+    path.push("target");
+    path.push("release");
+    if !path.exists() {
+        // probably not benchmarking - `cargo test` was invoked instead
+        eprintln!("{:?} doesn't exist - ignoring benchmark", path);
+        return;
+    }
+    path.push("psbattletools");
 
     let mut cmd = Command::new(&path);
     cmd.arg("search").arg("AnniKa").arg(&*TEST_ROOT_DIR);
